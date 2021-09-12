ac3f573d98bea7fdf40da192f1ec827cb1e8356d
Gerdin <gerdinventura2@gmail.com>
2021-09-11 20:54:38 -0400

    initial commit

---
+++ Unit1/YelpCell.swift
@@ -0,0 +1,23 @@
+//
+//  YelpCell.swift
+//  Unit1
+//
+//  Created by Gerdin Ventura on 9/11/21.
+//
+
+import UIKit
+
+class YelpCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
